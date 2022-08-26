35d25d5563216ad2beb7c0ce24169f114313fdb4
Jussier Alves <jussier.alves@gmail.com>
2022-08-26 17:07:22 -0300

    Update template

---
+++ index/script/table.js
@@ -0,0 +1,11 @@
+class Table {
+    #tableHead;
+    #tableBody;
+    #tableColumn;
+    #tableRow;
+    #tableData;
+    constructor(tableHead, tableBody) {
+        this.#tableHead = tableHead; this.#tableBody = tableBody;
+    }
+    
+}
